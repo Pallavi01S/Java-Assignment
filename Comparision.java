220c55a5b90077d32f0a9a31650d3f56acec522e
Pallavi S <pallavi01suresh@gmail.com>
2023-07-11 23:37:37 +0530

    Assignment
    Daily assignments

---
+++ Comparision.java
@@ -0,0 +1,15 @@
+class Comparision{
+public static void main(String []args){
+int a=10;
+int b=20;
+int c=5;
+int d=2;
+System.out.println("Equal Result : "+(a==b));
+System.out.println("UnEqual Result : "+(b!=a));
+System.out.println("Greater Than Result : "+(c>d));
+System.out.println("Lesser Than Result : "+(a<d));
+System.out.println("Greater Than or EqualTo Result : "+(c>=d));
+System.out.println("Lesser Than or EqualTo Result : "+(a<=d));
+}
+}
+
